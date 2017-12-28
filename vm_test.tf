e1915cf72ef05312ab27840936e67187e289da98
owhc <owenc.au@gmail.com>
2017-12-28 17:06:20 +0800

    configuration fixed and add varibles

--- vm_test.tf
+++ vm_test.tf
@@ -1,13 +1,13 @@
 resource "ibm_compute_vm_instance" "twc_terraform_sample" {
-    hostname = "twc-terraform-sample-name"
-    domain = "oc.example.com"
-    os_reference_code = "CENTOS_7_64"
-    datacenter = "hkg02"
+    hostname = "${var.vm-hostname}"
+    domain = "${var.vm-domain}"
+    os_reference_code = "${var.vm-os-reference-code}"
+    datacenter = "${var.datacenter}"
     network_speed = 10
     hourly_billing = true
     private_network_only = false
-    cores = 1
-    memory = 1024
+    cores = ${var.vm-cores}
+    memory = ${var.vm-memory}
     disks = [25]
     user_metadata = "{\"value\":\"newvalue\"}"
     dedicated_acct_host_only = false
