59be66a13c47bcca52a6f6e9418f1da87f3dce8d
owhc <owenc.au@gmail.com>
2017-12-28 18:17:46 +0800

    test try

--- vm_test.tf
+++ vm_test.tf
@@ -6,8 +6,10 @@ resource "ibm_compute_vm_instance" "twc_terraform_sample" {
     network_speed = 10
     hourly_billing = true
     private_network_only = false
-    cores = ${var.vm-cores}
-    memory = ${var.vm-memory}
+    /* cores = ${var.vm-cores} */
+    cores = 1
+    /* memory = ${var.vm-memory} */
+    memory = 1024
     disks = [25]
     user_metadata = "{\"value\":\"newvalue\"}"
     local_disk = false
