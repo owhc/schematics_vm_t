4b199c63962a8a9463a952f8becbe5c76d7b0a0a
owhc <owenc.au@gmail.com>
2018-01-09 11:02:48 +0800

    Create tf file for new lbaas

---
+++ integrated_env.tf
@@ -0,0 +1,54 @@
+resource "ibm_compute_vm_instance" "vm_instances" {
+    count="2"
+    hostname = "${var.vm-hostname}"
+    domain = "${var.vm-domain}"
+    os_reference_code = "${var.vm-os-reference-code}"
+    datacenter = "${var.datacenter}"
+    network_speed = 10
+    hourly_billing = true
+    private_network_only = false
+    /* cores = ${var.vm-cores} */
+    cores = 1
+    /* memory = ${var.vm-memory} */
+    memory = 1024
+    disks = [25]
+    user_metadata = "{\"value\":\"newvalue\"}"
+    local_disk = false
+    public_vlan_id = 2200297
+    private_vlan_id = 2190899
+    private_security_group_ids = [241753,241755]
+    public_security_group_ids = [241749]
+}
+
+resource "ibm_lbaas" "lbaas" {
+  name        = "ibmCloudLBS"
+  description = "delete this plz"
+  subnets     = [974315]
+
+  protocols = [{
+    frontend_protocol     = "HTTPS"
+    frontend_port         = 443
+    backend_protocol      = "HTTP"
+    backend_port          = 80
+    load_balancing_method = "round_robin"
+    tls_certificate_id    = 11670
+  },
+    {
+      frontend_protocol     = "HTTP"
+      frontend_port         = 80
+      backend_protocol      = "HTTP"
+      backend_port          = 80
+      load_balancing_method = "round_robin"
+    },
+  ]
+
+  server_instances = [
+    {
+      "private_ip_address" = "${ibm_compute_vm_instance.vm_instances.0.ipv4_address_private}"
+    },
+    {
+      "private_ip_address" = "${ibm_compute_vm_instance.vm_instances.1.ipv4_address_private}"
+    },
+  ]
+}
+
